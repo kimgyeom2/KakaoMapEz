2ab5a08a08e8e6575f5ac593d86bd7fd8f696fbf
kim gyeom <kimgyeom2@gmail.com>
2023-10-26 17:00:27 +0900

    Initial commit

---
+++ app/src/main/java/com/gy25m/mapfinal/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.gy25m.mapfinal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mapView = findViewById<MapView>(R.id.map_view)
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+                // 지도 API 가 정상적으로 종료될 때 호출됨
+            }
+
+            override fun onMapError(error: Exception) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출됨
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+                // 인증 후 API 가 정상적으로 실행될 때 호출됨
+            }
+        })
+    }
+
+}
