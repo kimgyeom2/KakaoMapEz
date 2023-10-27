d805f6d506d88e0f27c2d70089548228da52aaeb
kim gyeom <kimgyeom2@gmail.com>
2023-10-27 14:05:23 +0900

    마커이미지 여기 안에서 만든거로 하면 안됨

--- app/src/main/java/com/gy25m/mapfinal/MainActivity.kt
+++ app/src/main/java/com/gy25m/mapfinal/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.gy25m.mapfinal
 
+
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
+import com.kakao.vectormap.animation.Animation
+import com.kakao.vectormap.label.Label
+import com.kakao.vectormap.label.LabelLayer
+import com.kakao.vectormap.label.LabelOptions
+import com.kakao.vectormap.label.LabelStyle
+import com.kakao.vectormap.label.LabelStyles
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,9 +33,35 @@ class MainActivity : AppCompatActivity() {
             }
         }, object : KakaoMapReadyCallback() {
             override fun onMapReady(kakaoMap: KakaoMap) {
-                // 인증 후 API 가 정상적으로 실행될 때 호출됨
+
+                var styles = LabelStyles.from(
+                    "myStyleId",
+                    LabelStyle.from(R.drawable.green_marker).setZoomLevel(8),
+                    LabelStyle.from(R.drawable.choonsik).setZoomLevel(11),
+                    LabelStyle.from(R.drawable.green_marker)
+                        .setTextStyles(32, Color.BLACK, 1, Color.GRAY).setZoomLevel(15)
+                )
+
+                styles = kakaoMap.labelManager!!.addLabelStyles(styles!!)
+
+                val label = kakaoMap.labelManager!!.layer!!.addLabel(
+                    LabelOptions.from(LatLng.from(37.476216,126.973209))
+                        .setStyles(styles).setTexts("Gyeom")
+                )
+
+                kakaoMap.setOnLabelClickListener { kakaoMap, layer, label ->
+                    Toast.makeText(this@MainActivity, label.texts[0], Toast.LENGTH_SHORT).show()
+                }
+
             }
+
+
+
+
         })
+
+
     }
+
 
 }
